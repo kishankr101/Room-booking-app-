3a693ea1a0b939d90749ba88194a4f98d37fc79a
Kishan Kumar <kishansingh74820@gmail.com>
2026-03-27 00:37:58 +0530

    Create data.py

---
+++ data.py
@@ -0,0 +1,22 @@
+mock_buildings = [
+    {
+        "id": 1, 
+        "name": "OYO Townhouse Student Wing", 
+        "region": "Delhi", 
+        "price": 7500, 
+        "features": "AC, WiFi, Meals", 
+        "empty_rooms": 8, 
+        "full_rooms": 42,
+        "image": "https://images.unsplash.com/photo-1555854877-bab0e564b8d5?w=600"
+    },
+    {
+        "id": 2, 
+        "name": "The Luxury Family Suite", 
+        "region": "Gurgaon", 
+        "price": 18000, 
+        "features": "Parking, Kitchen, Garden", 
+        "empty_rooms": 2, 
+        "full_rooms": 18,
+        "image": "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?w=600"
+    }
+]
